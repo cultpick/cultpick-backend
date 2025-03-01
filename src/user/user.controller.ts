f3ef56cb1383b5b812bf812ce059d1d9c91aa1af
aptheparker <apapap1119@g.skku.edu>
2025-03-01 18:05:20 +0900

    feat: 내 정보 조회 api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
+import { CurrentUser } from 'src/common/decorator/current-user.decorator';
+import { UserInfo } from 'src/auth/type';
 
 @ApiTags('User (유저)')
+@ApiBearerAuth('access-token')
+@UseGuards(JwtGuard)
 @Controller('/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @ApiOperation({
-    summary: '유저 정보 조회',
+    summary: '내 정보 조회',
   })
   @Get()
-  async getUser() {}
+  async getUser(@CurrentUser() user: UserInfo) {
+    return await this.userService.getUser(user);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'prisma/prisma.service';
+import { UserInfo } from 'src/auth/type';
 
 @Injectable()
 export class UserService {
-  async getUser() {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getUser(user: UserInfo): Promise<User> {
+    const existingUser = await this.prismaService.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+
+    return existingUser;
+  }
 }
