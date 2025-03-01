42b23c74a06fa6a0562b8be64493c30ece2fdd69
aptheparker <apapap1119@g.skku.edu>
2025-03-01 18:10:24 +0900

    fix: 내 정보 조회 api에 관심 카테고리도 반환

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Put, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
@@ -15,7 +15,7 @@ export class UserController {
   @ApiOperation({
     summary: '내 정보 조회',
   })
-  @Get()
+  @Get('/')
   async getUser(@CurrentUser() user: UserInfo) {
     return await this.userService.getUser(user);
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
 import { PrismaService } from 'prisma/prisma.service';
 import { UserInfo } from 'src/auth/type';
 
@@ -7,13 +6,23 @@ import { UserInfo } from 'src/auth/type';
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getUser(user: UserInfo): Promise<User> {
+  async getUser(user: UserInfo) {
     const existingUser = await this.prismaService.user.findUnique({
       where: {
         id: user.id,
       },
     });
 
-    return existingUser;
+    const favoriteCategoryList =
+      await this.prismaService.userToCategory.findMany({
+        where: {
+          userId: user.id,
+        },
+      });
+
+    return {
+      ...existingUser,
+      favoriteCategoryList,
+    };
   }
 }
