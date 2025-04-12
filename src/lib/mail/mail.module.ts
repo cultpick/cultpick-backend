52da6e84c0bd9591e9b57b9b3b128ec6d25dbf7e
aptheparker <apapap1119@g.skku.edu>
2025-04-12 10:40:31 +0900

    feat: handlebars template 수정

--- src/lib/mail/mail.module.ts
+++ src/lib/mail/mail.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { MailService } from './mail.service';
+import * as path from 'path';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { MailService } from './mail.service';
         from: `CultPick 컬픽 <${process.env.SMTP_USER}>`,
       },
       template: {
-        dir: __dirname + '/template',
+        dir: path.join(__dirname, 'template'),
         adapter: new HandlebarsAdapter(),
         options: {
           strict: true,
